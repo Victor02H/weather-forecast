f41eced640e2b1f28bdc3101496d455c583b812c
Victor02H <victor-silva02@outlook.com>
2022-05-11 13:36:10 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+
+import weatherApi from './services/weatherApi';
+import CityCard from './components/CityCard';
+import apiKey from './config/apiKey.json';
+
 import './App.css';
+import './responsive.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [weatherCards, setWeatherCards] = useState([]);
+
+  function searchCity(e) {
+    e.preventDefault();
+
+    weatherApi.get(`?q=${inputValue}&units=metric&appid=${apiKey.key}`)
+      .then(res => {
+
+        if (weatherCards.length <= 3) {
+          toast.success('Success!');
+          setWeatherCards([...weatherCards, res.data]);
+
+          return;
+        }
+
+        toast.info('Reached the maximum!');
+      })
+      .catch(err => {
+        toast.error('Error!');
+        console.log(err);
+      })
+
+    return;
+  }
+
+  function resetWeatherCards() {
+    setWeatherCards([]);
+
+    toast.info('All cards removed!');
+
+    return;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Weather App</h1>
+
+      <form>
+        <div className='search-container'>
+          <input
+            type='text'
+            name='search-city'
+            autoComplete='off'
+            placeholder='Search city'
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+
+          <input
+            type='submit'
+            name='submit'
+            value='Submit'
+            onClick={(e) => searchCity(e)}
+          />
+        </div>
+
+        <input
+          type='button'
+          name='reset'
+          value='Reset'
+          onClick={() => resetWeatherCards()}
+        />
+      </form>
+
+      <div className='align-city-cards'>
+        {
+          weatherCards.map((item, index) => {
+            return (
+              <CityCard
+                key={index}
+                cityName={item.name}
+                cardName={weatherCards}
+                countryName={item.sys.country}
+                weatherDescription={item.weather[0].description}
+                temp={item.main}
+                weather={item.weather[0].main}
+                timezone={item.timezone}
+              />
+            );
+          })
+        }
+      </div>
+
+
+      <ToastContainer
+        position='top-right'
+        theme='colored'
+      />
     </div>
   );
 }
